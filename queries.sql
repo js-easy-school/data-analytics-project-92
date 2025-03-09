b1e28e1d2368261bd6e8081e28c0c913c6fc737e
Andrey Mazunin <97140182+js-easy-school@users.noreply.github.com>
2025-03-09 20:07:56 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -44,3 +44,45 @@ GROUP BY
     LOWER(TRIM(TO_CHAR(s.sale_date, 'Day'))), -- Шаг 2: Группируем по дню недели
     EXTRACT(ISODOW FROM s.sale_date) -- Шаг 2: Группируем по порядковому номеру дня недели
 ORDER BY EXTRACT(ISODOW FROM s.sale_date), seller; -- Шаг 3: Сортируем по дню недели и продавцу
+
+-- Отчет 1: Количество покупателей в разных возрастных группах
+SELECT 
+    CASE 
+        WHEN c.age BETWEEN 16 AND 25 THEN '16-25'
+        WHEN c.age BETWEEN 26 AND 40 THEN '26-40'
+        ELSE '40+'
+    END AS age_category,
+    COUNT(DISTINCT c.customer_id) AS age_count
+FROM customers c
+GROUP BY age_category
+ORDER BY age_category;
+
+-- Отчет 2: Количество покупателей и выручка по месяцам
+SELECT 
+    TO_CHAR(s.sale_date, 'YYYY-MM') AS selling_month,
+    COUNT(DISTINCT s.customer_id) AS total_customers,
+    ROUND(SUM(p.price * s.quantity)) AS income
+FROM sales s
+JOIN products p ON s.product_id = p.product_id
+GROUP BY selling_month
+ORDER BY selling_month;
+
+-- Отчет 3: Покупатели, первая покупка которых была акционной (цена = 0)
+WITH first_purchases AS (
+    SELECT 
+        s.customer_id,
+        MIN(s.sale_date) AS first_sale_date
+    FROM sales s
+    JOIN products p ON s.product_id = p.product_id
+    WHERE p.price = 0
+    GROUP BY s.customer_id
+)
+SELECT 
+    CONCAT(c.first_name, ' ', c.last_name) AS customer,
+    fp.first_sale_date AS sale_date,
+    CONCAT(e.first_name, ' ', e.last_name) AS seller
+FROM first_purchases fp
+JOIN sales s ON fp.customer_id = s.customer_id AND fp.first_sale_date = s.sale_date
+JOIN customers c ON fp.customer_id = c.customer_id
+JOIN employees e ON s.sales_person_id = e.employee_id
+ORDER BY c.customer_id;
